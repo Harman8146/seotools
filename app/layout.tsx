b0a0308ae9d86aeeb632973e0b88e258711c2b37
Harmanjot Singh <98830878+Harman8146@users.noreply.github.com>
2026-01-30 21:27:59 +0530

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-<meta name="google-site-verification" content="TYDPejaagQSNZIRilcDbgSHd0CPqM7dxnOK7UpRT8yg" />
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
